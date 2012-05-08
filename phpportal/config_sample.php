52560cc8b6c128a2b70391b7bd13e1728c1043e1
robertmlowe <robertmlowe@c3fd45b7-fe04-47ee-63fb-0da86bac8117>
2012-05-08 09:36:22 +0000

    config_sample.php

---
+++ phpportal/config_sample.php
@@ -0,0 +1,18 @@
+<?php
+
+// Make a copy of this file named config.php and edit as appropriate.
+
+// Base URL of the Talent Suite site
+$ekp_base = 'http://www.example.com/ekp/';
+
+// Value of the authentication.key property in ekp.properties
+$auth_key = 'my_secret_key';
+
+// User ID and password of a privileged user (e.g. System Administrator)
+$admin_user_name = 'phpportal';
+$admin_password = 'my_secret_password';
+
+// Absolute URL for the PHP authentication callback script
+$auth_callback = 'http://localhost/phpportal/auth_callback.php';
+
+?>
